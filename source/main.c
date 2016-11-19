22c6559d89fea421ba28131f3d6daefb6f06194a
TheReturningVoid <TheReturningVoid@users.noreply.github.com>
2016-11-19 15:08:45 +1100

    stuff

--- source/main.c
+++ source/main.c
@@ -27,29 +27,13 @@ void __appInit() {
 int main(int argc, char **argv) {
 
     printf("PestMenu v0.0.1\n");
+    srvGetServiceHandle(nssHandle, "ns:s");
     printf("APT_Initialize: %08X\n", APT_Initialize(APPID_HOMEMENU, aptMakeAppletAttr(APTPOS_SYS, true, true), signalEvent, resumeEvent));
 
     while (aptMainLoop()) {
       hidScanInput();
-      //if (hidKeysDown() & KEY_START) break;
 
       if (hidKeysDown() & KEY_A) {
-
-        //u32 *prep_leave_hm_cmdbuf = getThreadCommandBuffer();
-        //prep_leave_hm_cmdbuf[0] = IPC_MakeHeader(0x2D,0,0); // 0x2D0000, APT_PrepareToLeaveHomeMenu
-        //ret = aptSendCommand(prep_leave_hm_cmdbuf);
-        //printf("APT_PrepareToLeaveHomeMenu: %08X\n", ret);
-        //clearCmdBuf();
-
-        //u32 *leave_hm_cmdbuf = getThreadCommandBuffer();
-        //leave_hm_cmdbuf[0] = IPC_MakeHeader(0x2E,1,4); // 0x2E0044, APT_LeaveHomeMenu
-        //leave_hm_cmdbuf[1] = sizeof(hmxParam);
-        //leave_hm_cmdbuf[2] = 0;
-        //leave_hm_cmdbuf[3] = aptHandle;
-        //leave_hm_cmdbuf[4] = (sizeof(hmxParam) << 14) | 2;
-        //leave_hm_cmdbuf[5] = hmxParam;
-        //ret = aptSendCommand(leave_hm_cmdbuf);
-        //printf("APT_LeaveHomeMenu: %08X\n", ret);
 
         FS_ProgramInfo inf = {0x000400000FF40002, MEDIATYPE_NAND};
         u32 prep_start_app_cmdbuf[16];
